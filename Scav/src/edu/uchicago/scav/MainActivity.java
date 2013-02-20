11c3a9cbf45041a1d2f361f03078239612071b0d
Anton Osten <anton@ostensible.me>
2013-02-19 20:22:00 -0600

    - login screen

--- Scav/src/edu/uchicago/scav/MainActivity.java
+++ Scav/src/edu/uchicago/scav/MainActivity.java
@@ -1,22 +1,45 @@
 package edu.uchicago.scav;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends Activity
+{
+	
+	final String PREFS_NAME = "MyPrefsFile";
+			
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+    	
+    	if (settings.getBoolean("first_launch", true)) 
+    	{
+    		Intent login = new Intent(MainActivity.this, LoginActivity.class);
+    		startActivity(login);
+    		settings.edit().putBoolean("first_launch", false).commit();
+    	}
+    	setContentView(R.layout.activity_main);
     }
-
+    
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+    
+    public void firstLaunchClick(View view)
+    {
+    	SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+    	settings.edit().putBoolean("first_launch", true).commit();
+    	android.os.Process.killProcess(android.os.Process.myPid());
     }
     
 }
